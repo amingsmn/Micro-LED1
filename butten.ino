dcc658473545e98117d0bdd5b77e9dca95bba9ae
amingsmn <amingsmn@gmail.com>
2025-03-04 09:51:20 -0800

    Add files via upload

---
+++ butten.ino
@@ -0,0 +1,16 @@
+int led = 13;
+int button = 7;
+int state = 0;
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(button, INPUT);
+}
+
+void loop() {
+  state = digitalRead(button);
+  if (state == HIGH) {
+    digitalWrite(led, HIGH);
+  } else {
+    digitalWrite(led, LOW);
+  }
+}
